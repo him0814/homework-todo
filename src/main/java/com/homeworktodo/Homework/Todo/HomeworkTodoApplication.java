26fbf5f6b7bd83b254489588a0460ab0a9027ff4
him0814 <him1325@gmail.com>
2023-06-12 14:08:27 +0900

    todo homework test 1

---
+++ src/main/java/com/homeworktodo/Homework/Todo/HomeworkTodoApplication.java
@@ -0,0 +1,13 @@
+package com.homeworktodo.Homework.Todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homeworktodo/Homework/Todo/HomeworkTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homeworktodo.Homework.Todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
